858f35793a96c9621893338215b8d8a02a9725e3
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2021-02-01 14:39:34 -0300

    Modal e logica de busca do localStorage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Income from '../../assets/income.svg';
 import Expense from '../../assets/expense.svg';
 import Total from '../../assets/total.svg';
@@ -6,119 +6,152 @@ import Minus from '../../assets/minus.svg';
 import Header from '../../components/Header';
 import Card from '../../components/Card';
 
-const App: React.FC = () => (
-  <>
-    <Header />
-    <main className="container">
-      <section id="balance">
-        <h2 className="sr-only">Balanço</h2>
-        <Card type="Entradas" img={Income} value="R$ 5.000,00" />
-        <Card type="Saidas" img={Expense} value="R$ 2.000,00" />
-        <Card type="Total" img={Total} value="R$ 3.000,00" />
-      </section>
+interface Transaction {
+  description: string;
+  value: string;
+  date: string;
+}
 
-      <section id="transaction">
-        <h2 className="sr-only">Transações</h2>
+const App: React.FC = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+  const [date, setDate] = useState('');
+  const [transactions, setTransactions] = useState<Transaction[]>(() => {
+    const storagedTransactions = localStorage.getItem('@Finances:transactions');
 
-        <a href="#" className="button new">
-          + Nova Transação
-        </a>
+    if (storagedTransactions) {
+      return JSON.parse(storagedTransactions);
+    }
+    return [];
+  });
 
-        <table id="data-table">
-          <thead>
-            <tr>
-              <th>Descrição</th>
-              <th>Valor</th>
-              <th>Data</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="description">Luz</td>
-              <td className="expense">- R$ 500,00</td>
-              <td className="date">23/01/2021</td>
-              <td>
-                <img src={Minus} alt="Remover transação" />
-              </td>
-            </tr>
-            <tr>
-              <td className="description">Criação website</td>
-              <td className="income">R$ 5.000,00</td>
-              <td className="date">23/01/2021</td>
-              <td>
-                <img src={Minus} alt="Remover transação" />
-              </td>
-            </tr>
-            <tr>
-              <td className="description">Internet</td>
-              <td className="expense">- R$ 200,00</td>
-              <td className="date">23/01/2021</td>
-              <td>
-                <img src={Minus} alt="Remover transação" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-    </main>
+  useEffect(() => {
+    localStorage.setItem(
+      '@Finances:transactions',
+      JSON.stringify(transactions),
+    );
+  }, [transactions]);
 
-    <div className="modal-overlay">
-      <div className="modal">
-        <div id="form">
-          <h2>Nova Transação</h2>
-          <form action="">
-            <div className="input-group">
-              <label className="sr-only" htmlFor="description">
-                Descrição
-              </label>
-              <input
-                type="text"
-                id="description"
-                name="description"
-                placeholder="Descrição"
-              />
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <section id="balance">
+          <h2 className="sr-only">Balanço</h2>
+          <Card type="Entradas" img={Income} value="R$ 5.000,00" />
+          <Card type="Saidas" img={Expense} value="R$ 2.000,00" />
+          <Card type="Total" img={Total} value="R$ 3.000,00" />
+        </section>
+
+        <section id="transaction">
+          <h2 className="sr-only">Transações</h2>
+
+          <button type="button" onClick={() => setModalVisible(true)}>
+            + Nova Transação
+          </button>
+
+          <table id="data-table">
+            <thead>
+              <tr>
+                <th>Descrição</th>
+                <th>Valor</th>
+                <th>Data</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map(transaction => (
+                <tr key={description}>
+                  <th>{description}</th>
+                  <th>{value}</th>
+                  <th>{date}</th>
+                  <th />
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
+      </main>
+      {modalVisible ? (
+        <div className="modal-overlay">
+          <div className="modal">
+            <div id="form">
+              <h2>Nova Transação</h2>
+              <form action="">
+                <div className="input-group">
+                  <label className="sr-only" htmlFor="description">
+                    Descrição
+                  </label>
+                  <input
+                    type="text"
+                    id="description"
+                    name="description"
+                    placeholder="Descrição"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                  />
+                </div>
+
+                <div className="input-group">
+                  <label className="sr-only" htmlFor="amount">
+                    Valor
+                  </label>
+                  <input
+                    type="number"
+                    step="0.01"
+                    id="amount"
+                    name="amount"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                    placeholder="0,00"
+                  />
+                  <small className="help">
+                    Use o sinal - (negativo) para despesas e , (vírgula) para
+                    casas decimais
+                  </small>
+                </div>
+
+                <div className="input-group">
+                  <label className="sr-only" htmlFor="date">
+                    Data
+                  </label>
+                  <input
+                    type="date"
+                    id="date"
+                    name="date"
+                    value={date}
+                    onChange={e => setDate(e.target.value)}
+                  />
+                </div>
+
+                <div className="input-group actions">
+                  <button
+                    type="button"
+                    onClick={() => setModalVisible(false)}
+                    className="button cancel"
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    type="submit"
+                    onClick={() => setModalVisible(false)}
+                    className="button cancel"
+                  >
+                    Salvar
+                  </button>
+                </div>
+              </form>
             </div>
-
-            <div className="input-group">
-              <label className="sr-only" htmlFor="amount">
-                Valor
-              </label>
-              <input
-                type="number"
-                step="0.01"
-                id="amount"
-                name="amount"
-                placeholder="0,00"
-              />
-              <small className="help">
-                Use o sinal - (negativo) para despesas e , (vírgula) para casas
-                decimais
-              </small>
-            </div>
-
-            <div className="input-group">
-              <label className="sr-only" htmlFor="date">
-                Data
-              </label>
-              <input type="date" id="date" name="date" />
-            </div>
-
-            <div className="input-group actions">
-              <a href="#" className="button cancel">
-                Cancelar
-              </a>
-              <button type="button">Salvar</button>
-            </div>
-          </form>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : null}
 
-    <footer>
-      <p>dev.finance$</p>
-    </footer>
-  </>
-);
+      <footer>
+        <p>dev.finance$</p>
+      </footer>
+    </>
+  );
+};
 
 export default App;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -169,19 +169,12 @@ td.expense {
 .modal-overlay {
   width: 100%;
   height: 100%;
-
   background-color: rgba(0,0,0,0.7);
-
   position: fixed;
   top: 0;
-
   display:flex;
   align-items: center;
   justify-content: center;
-
-  opacity: 0;
-  visibility: hidden;
-
   z-index: 999;
 }
 
